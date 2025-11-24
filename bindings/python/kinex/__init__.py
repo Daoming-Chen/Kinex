347c1524dbc5b93b666352d5bb6f6c0667f33cfa
daoming chen <chendaoming99@outlook.ie>
2025-11-25 00:20:47 +0800

    Fix Python bindings installation: add DLL directory on Windows, remove library versioning for Python builds, set RPATH correctly

--- bindings/python/kinex/__init__.py
+++ bindings/python/kinex/__init__.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+if os.name == 'nt':
+    # On Windows, we need to explicitly add the directory containing the DLLs
+    # to the DLL search path for Python 3.8+
+    # The DLLs are located in the same directory as this __init__.py
+    kinex_dir = os.path.dirname(__file__)
+    os.add_dll_directory(kinex_dir)
+
 from ._kinex import *
 from ._kinex import __version__
 
